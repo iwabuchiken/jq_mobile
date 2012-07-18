87168029e72de7741a454d2fe3cdc5c55407e0cd
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-07-18 23:43:03 +0900

    master
    v-1.0
    
    Initial
    
    Signed-off-by: iwabuchiken <iwabuchi.k.2010@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqMobile::Application.config.secret_token = '79f300ca72d5948997d9fcb092d7ec6abb0c68f17baa6789ad3c995d0d227194455643d5df582a5faa9a8ce3cc794722889ed946f8643d1a484734c7624c4469'
